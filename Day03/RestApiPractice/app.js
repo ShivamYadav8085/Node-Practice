dd8585b06d23d907624173902a741cf82685035a
Shivam Yadav <Shivamy2@mobiquityinc.com>
2023-07-14 00:07:49 +0530

    added logic for response formatting

--- Day03/RestApiPractice/app.js
+++ Day03/RestApiPractice/app.js
@@ -1,6 +1,8 @@
 import express, { json, urlencoded } from "express";
 import { connect } from "mongoose";
 import "dotenv/config.js";
+import _ from "lodash";
+import status from "statuses";
 import logger from "morgan";
 import { router as bookRouter } from "./src/routes/book.js";
 import { router as userRouter } from "./src/routes/user.js";
@@ -19,12 +21,28 @@ app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  const originalJson = res.json;
+  res.json = (data) => {
+    if (!data.error) {
+      const formattedResponse = {
+        status: status(res.statusCode),
+        data: _.isArray(data) ? [...data] : _.isObject(data) ? [data] : [],
+        error: null,
+      };
+      return originalJson.call(res, formattedResponse);
+    }
+    return originalJson.call(res, data);
+  };
+  next();
+});
+
 app.use("/books", bookRouter);
 app.use("/user", userRouter);
 
 app.use((err, req, res, next) => {
   res.status(err.status).json({
-    status: "error",
+    status: status(res.statusCode),
     data: [],
     error: err.message,
   });
