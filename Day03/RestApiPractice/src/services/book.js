f9329c2b117406f575cc77cc709bcb410b8eea2a
shivay10th <shivay9455@gmail.com>
2023-07-16 13:18:19 +0530

    refactored

--- Day03/RestApiPractice/src/services/book.js
+++ Day03/RestApiPractice/src/services/book.js
@@ -1,75 +1,75 @@
-import _ from "lodash";
-import { Book } from "../shared/database/Models/book.js";
-
+import _ from 'lodash';
+import { Book } from '../shared/database/models/book.js';
 
 const getAllBooks = async () => {
-  try {
-    const allBooks = await Book.find();
-    return allBooks;
-  } catch (error) {
-    throw error;
-  }
+	try {
+		const allBooks = await Book.find();
+		return allBooks;
+	} catch (error) {
+		throw error;
+	}
 };
 
 const getBookById = async (bookId) => {
-  try {
-    const book = await Book.findById(bookId);
-    if(book){
-      return book;
-    }
-    const notFoundError = new Error(`Book with id:${bookId} is not present`);
-    notFoundError.name="NotFoundError"
-    throw notFoundError
-  } catch (error) {
-    throw error
-  }
+	try {
+		const book = await Book.findById(bookId);
+		if (book) {
+			return book;
+		}
+		const notFoundError = new Error(
+			`Book with id:${bookId} is not present`,
+		);
+		notFoundError.name = 'NotFoundError';
+		throw notFoundError;
+	} catch (error) {
+		throw error;
+	}
 };
 
 const getBooksByAuthor = async (authorName) => {
-  try {
-    const booksOfAuthor =await  Book.find({author:authorName})
-    if(booksOfAuthor.length){
-      return booksOfAuthor;
-    }
+	try {
+		const booksOfAuthor = await Book.find({ author: authorName });
+		if (booksOfAuthor.length) {
+			return booksOfAuthor;
+		}
 
-    const notFoundError = new Error(`Book of author:${authorName} is not present`);
-    notFoundError.name="NotFoundError"
-    throw notFoundError
-
-  } catch (error) {
-    throw error;
-  }
+		const notFoundError = new Error(
+			`Book of author:${authorName} is not present`,
+		);
+		notFoundError.name = 'NotFoundError';
+		throw notFoundError;
+	} catch (error) {
+		throw error;
+	}
 };
 
 const createABook = async (newBook) => {
-  try {
-    const response = await Book.create(newBook);
-    return response;
-  } catch (error) {
-    throw error;
-  }
+	try {
+		const response = await Book.create(newBook);
+		return response;
+	} catch (error) {
+		throw error;
+	}
 };
 
-const deleteBookById = async (bookId)=>{
-  try {
-    const response = await Book.deleteOne({_id:bookId})
-    if (response.deletedCount) {
-      return response.deletedCount
-    }
-    const notFoundError = new Error(`Book of id:${bookId} is not present`);
-    notFoundError.name="NotFoundError"
-    throw notFoundError
-  } catch (error) {
-    throw error;
-  }
-}
-
-
+const deleteBookById = async (bookId) => {
+	try {
+		const response = await Book.deleteOne({ _id: bookId });
+		if (response.deletedCount) {
+			return response.deletedCount;
+		}
+		const notFoundError = new Error(`Book of id:${bookId} is not present`);
+		notFoundError.name = 'NotFoundError';
+		throw notFoundError;
+	} catch (error) {
+		throw error;
+	}
+};
 
 export {
-  getAllBooks,
-  getBookById,
-  getBooksByAuthor,
-  createABook,
-  deleteBookById
+	getAllBooks,
+	getBookById,
+	getBooksByAuthor,
+	createABook,
+	deleteBookById,
 };
--- Day03/RestApiPractice/src/shared/middleware/validation/validateRegister.js
+++ Day03/RestApiPractice/src/shared/middleware/validation/validateRegister.js
@@ -1,17 +1,17 @@
-import createError from "http-errors"
+import createError from 'http-errors';
 
-import {userRegisterSchema} from "../../validation/userRegisterSchema.js"
+import { userRegisterSchema } from '../../validation/userRegisterSchema.js';
 
-const validateUserRegister = (req,res)=>{
-    const {error}=userRegisterSchema.validate(req.body)
-    if(error){
-        const {details} = error
-        console.log(error);
-        console.log(details);
-        const message = details.map(errorDetail => errorDetail.message.replace(/['"]/g, '')).join(',')
-        next(createError(422,message))
-    }else{
-        next();
-    }
-}
-export {validateUserRegister}
+const validateUserRegister = (req, res, next) => {
+	const { error } = userRegisterSchema.validate(req.body);
+	if (error) {
+		const { details } = error;
+		const message = details
+			.map((errorDetail) => errorDetail.message.replace(/['"]/g, ''))
+			.join(',');
+		next(createError(422, message));
+	} else {
+		next();
+	}
+};
+export { validateUserRegister };
